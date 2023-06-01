d5b3c785335047ab7e757e0ee06ec369d18727d2
Reiggnar <bruno.remy.sebastien@gmail.com>
2023-06-01 14:54:48 +0200

    final commit

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const btnModale = document.querySelector(".btn-modale")
 const btnClose = document.querySelector(".btn-close")
 const modale = document.querySelector(".modale")
+const modaleContent = document.querySelector(".modale-content")
+// const btns = document.querySelectorAll(".btn")
 
 btnModale.addEventListener("click", function () {
   modale.style.display = "block"
+  modaleContent.style.top = "50%"
 })
 btnClose.addEventListener("click", function () {
   modale.style.display = "none"
@@ -13,3 +16,13 @@ window.addEventListener("click", function (e) {
     modale.style.display = "none"
   }
 })
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", function () {
+//     if (modale.style.display == "none") {
+//       modale.style.display = "block"
+//     } else {
+//       modale.style.display = "none"
+//     }
+//   })
+// })
